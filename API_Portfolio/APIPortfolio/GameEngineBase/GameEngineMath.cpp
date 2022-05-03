f8024da6cd7b24027976433f0d663dc49e13973f
gmltkd0423 <54795962+gmltkd0423@users.noreply.github.com>
2022-05-03 12:53:11 +0900

    회전공식 업데이트

--- API_Portfolio/APIPortfolio/GameEngineBase/GameEngineMath.cpp
+++ API_Portfolio/APIPortfolio/GameEngineBase/GameEngineMath.cpp
@@ -1,8 +1,12 @@
 #include "GameEngineMath.h"
 
 const float GameEngineMath::PIE = 3.141592653589793238462643383279502884197169399375105820974944f;
+const float GameEngineMath::PIE2 = PIE * 2.0f;
+
 const float GameEngineMath::DEG = 180.0f;
 const float GameEngineMath::DegreeToRadian = PIE / DEG;
+
+const float GameEngineMath::RadianToDegree = DEG / PIE;
 
 
 GameEngineMath::GameEngineMath()
